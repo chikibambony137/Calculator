14c506a2d98aa6092faec07a3aca20bbfea5ad00
Antonchek <aboba@gmail.com>
2024-07-16 15:32:58 +0300

    Создать базовую логику калькулятора (консоль)

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
+import actions
+
 if (__name__ == "__main__"):
-    print("Hello world")
+    term: str = input("Введите выражение: ")
+    
+    for i in term:
+        if (i == "+"):
+            term_list = term.split("+")
+            x1: int = int(term_list[0])
+            x2: int = int(term_list[1])
+            actions.Add(x1, x2)
+        elif (i == "-"):
+            term_list = term.split("-")
+            x1: int = int(term_list[0])
+            x2: int = int(term_list[1])
+            actions.Sub(x1, x2)
+        elif (i == "*"):
+            term_list = term.split("*")
+            x1: int = int(term_list[0])
+            x2: int = int(term_list[1])
+            actions.Mult(x1, x2)
+        elif (i == "/"):
+            term_list = term.split("/")
+            x1: int = int(term_list[0])
+            x2: int = int(term_list[1])
+            actions.Div(x1, x2)
