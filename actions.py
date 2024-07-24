90169c0699b7b3f3c3a7cfbed9ff774593b99130
Antonchek <aboba@gmail.com>
2024-07-24 21:30:47 +0300

    Оптимизировать код основного модуля

--- actions.py
+++ actions.py
@@ -2,12 +2,15 @@
 def Add(a: int, b: int) -> int:
     result = a + b
     print(result)
+
 def Sub(a: int, b: int) -> int:
     result = a - b
     print(result)
+
 def Mult(a: int, b: int) -> int:
     result = a * b
     print(result)
+    
 def Div(a: int, b: int) -> float:
     result = a / b
     print(result)
--- main.py
+++ main.py
@@ -1,26 +1,18 @@
 import actions
 
+operations: dict = {
+        "+": actions.Add,
+        "-": actions.Sub,
+        "*": actions.Mult,
+        "/": actions.Div
+}
+
 if (__name__ == "__main__"):
     term: str = input("Введите выражение: ")
     
-    for i in term:
-        if (i == "+"):
-            term_list = term.split("+")
+    for op, func in operations.items():
+        if (op in term):
+            term_list = term.split(op)
             x1: int = int(term_list[0])
             x2: int = int(term_list[1])
-            actions.Add(x1, x2)
-        elif (i == "-"):
-            term_list = term.split("-")
-            x1: int = int(term_list[0])
-            x2: int = int(term_list[1])
-            actions.Sub(x1, x2)
-        elif (i == "*"):
-            term_list = term.split("*")
-            x1: int = int(term_list[0])
-            x2: int = int(term_list[1])
-            actions.Mult(x1, x2)
-        elif (i == "/"):
-            term_list = term.split("/")
-            x1: int = int(term_list[0])
-            x2: int = int(term_list[1])
-            actions.Div(x1, x2)
+            func(x1, x2)
