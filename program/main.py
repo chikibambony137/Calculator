37540e42fdc0e1deabfa5136039d47a07033b23a
Antonchek <aboba@gmail.com>
2024-07-30 21:16:04 +0300

    test: добавить бесконечный цикл

--- program/main.py
+++ program/main.py
@@ -1,31 +1,35 @@
 import actions, logger as l
 
 def main():
-    # action -> func
-    operations: dict = {
-            "+": actions.Add,
-            "-": actions.Sub,
-            "*": actions.Mult,
-            "/": actions.Div
-    }
-    l.logger.info("dict \"operations\" created")
+    while True:
+        # action -> func
+        operations: dict = {
+                "+": actions.Add,
+                "-": actions.Sub,
+                "*": actions.Mult,
+                "/": actions.Div
+        }
+        l.logger.info("dict \"operations\" created")
 
-    term: str = input("Введите выражение: ")
-    
-    l.logger.info("starting loop")
-    for op, func in operations.items():
-        if (op in term):
+        term: str = input("Введите выражение: ")
 
-            l.logger.debug(f"operation is \"{op}\"")
-            term_list = term.split(op)
-            try:
-                x1: float = float(term_list[0])
-                x2: float = float(term_list[1])
-                func(x1, x2)
-                l.logger.info("end program...")
-            except:
-                l.logger.error("Incorrect term")
-                print("Ошибка! Введено некорректное выражение!")
+        if (term == "q"):
+            break
+        
+        l.logger.info("starting loop")
+        for op, func in operations.items():
+            if (op in term):
+
+                l.logger.debug(f"operation is \"{op}\"")
+                term_list = term.split(op)
+                try:
+                    x1: float = float(term_list[0])
+                    x2: float = float(term_list[1])
+                    func(x1, x2)
+                    l.logger.info("end program...")
+                except:
+                    l.logger.error("Incorrect term")
+                    print("Ошибка! Введено некорректное выражение!")
 
 if (__name__ == "__main__"):
     l.logger.info("start program...")
