d404efcaa265fcc296724fb1682023f9cab2d1e0
Antonchek <aboba@gmail.com>
2024-07-30 22:33:20 +0300

    update: add new buttons and printing in textbox

--- program/window.py
+++ program/window.py
@@ -3,6 +3,10 @@ import customtkinter
 def button1_Click():
     print("Ughhh...")
 
+def printTerm(x: str):
+    textbox1.insert("end", x)
+    textbox1.update()
+
 if (__name__ == "__main__"):
     app = customtkinter.CTk()
     app.geometry("390x500+700+200")
@@ -17,35 +21,41 @@ if (__name__ == "__main__"):
     textbox1 = customtkinter.CTkTextbox(app, width=100, height=10)
     textbox1.grid(row=1, column=0, padx=0, pady=1, sticky="NSEW", columnspan=3)
     
-    button1 = customtkinter.CTkButton(app, text="1", width=130, command=button1_Click)
+    button1 = customtkinter.CTkButton(app, text="1", width=130, command=lambda: printTerm("1"))
     button1.grid(row=2, column=0, padx=0, pady=0, sticky="ew")
 
-    button2 = customtkinter.CTkButton(app, text="2", width=130, command=button1_Click)
+    button2 = customtkinter.CTkButton(app, text="2", width=130, command=lambda: printTerm("2"))
     button2.grid(row=2, column=1, padx=0, pady=0, sticky="ew")
 
-    button3 = customtkinter.CTkButton(app, text="3", width=130, command=button1_Click)
+    button3 = customtkinter.CTkButton(app, text="3", width=130, command=lambda: printTerm("3"))
     button3.grid(row=2, column=2, padx=0, pady=0, sticky="ew")
 
-    button4 = customtkinter.CTkButton(app, text="4", width=130, command=button1_Click)
+    button4 = customtkinter.CTkButton(app, text="4", width=130, command=lambda: printTerm("4"))
     button4.grid(row=3, column=0, padx=0, pady=0, sticky="ew")
 
-    button5 = customtkinter.CTkButton(app, text="5", width=130, command=button1_Click)
+    button5 = customtkinter.CTkButton(app, text="5", width=130, command=lambda: printTerm("5"))
     button5.grid(row=3, column=1, padx=0, pady=0, sticky="ew")
 
-    button6 = customtkinter.CTkButton(app, text="6", width=130, command=button1_Click)
+    button6 = customtkinter.CTkButton(app, text="6", width=130, command=lambda: printTerm("6"))
     button6.grid(row=3, column=2, padx=0, pady=0, sticky="ew")
 
-    button7 = customtkinter.CTkButton(app, text="7", width=130, command=button1_Click)
+    button7 = customtkinter.CTkButton(app, text="7", width=130, command=lambda: printTerm("7"))
     button7.grid(row=4, column=0, padx=0, pady=0, sticky="ew")
 
-    button8 = customtkinter.CTkButton(app, text="8", width=130, command=button1_Click)
+    button8 = customtkinter.CTkButton(app, text="8", width=130, command=lambda: printTerm("8"))
     button8.grid(row=4, column=1, padx=0, pady=0, sticky="ew")
 
-    button9 = customtkinter.CTkButton(app, text="9", width=130, command=button1_Click)
+    button9 = customtkinter.CTkButton(app, text="9", width=130, command=lambda: printTerm("9"))
     button9.grid(row=4, column=2, padx=0, pady=0, sticky="ew")
 
-    button0 = customtkinter.CTkButton(app, text="0", width=130, command=button1_Click)
+    button0 = customtkinter.CTkButton(app, text="0", width=130, command=lambda: printTerm("0"))
     button0.grid(row=5, column=1, padx=0, pady=0, sticky="ew")
+
+    buttonEq = customtkinter.CTkButton(app, text="=", width=130, command=lambda: printTerm("="))
+    buttonEq.grid(row=5, column=2, padx=0, pady=0, sticky="ew")
+
+    buttonSum = customtkinter.CTkButton(app, text="+", width=130, command=lambda: printTerm("+"))
+    buttonSum.grid(row=5, column=0, padx=0, pady=0, sticky="ew")
 
 
     app.mainloop()
